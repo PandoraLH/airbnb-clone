07f91caeeb78c5812ddf511a756e72f8f064f616
PandoraLH <lixhen2002@gmail.com>
2023-05-16 16:54:15 +0700

    UI register

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
+import ToasterProvider from "./providers/ToasterProvider";
 import "./globals.css";
 
 import { Nunito } from "next/font/google";
@@ -20,7 +23,11 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={font.className}>
-            <Navbar />
+            <ClientOnly>
+               <ToasterProvider />
+               <RegisterModal />
+               <Navbar />
+            </ClientOnly>
             {children}
          </body>
       </html>
